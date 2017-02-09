e8518a3ba0746edf94b934c67e59575573f94042
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2017-02-09 10:03:46 -0500

    Added comments explaining current logic. Next up may or may not be my 'rings' strategy.

--- mapperB.c
+++ mapperB.c
@@ -14,6 +14,8 @@ typedef struct Node
 
 double haversine(double lat1, double lon1, double lat2, double lon2)
 {
+    //Find distance between two points based on latitude and longitude.
+    //DOES NOT CURRENTLY ACCOUNT FOR ALTITUDE!
 	double dx, dy, dz;
 	lon1 -= lon2;
 	lon1 *= (3.1415926536 / 180), lat1 *= (3.1415926536 / 180), lat2 *= (3.1415926536 / 180);
@@ -26,10 +28,13 @@ double haversine(double lat1, double lon1, double lat2, double lon2)
 
 int checkAdjacency(node *a, node*b)
 {
+    //Wrapper for haversine, so that two nodes can be passed.
+    //For ease of use/typing, as it reduces the number of parameters.
+    //Also serves as a simplified yes/no check for "is in range."
     int dist = haversine(a->lat, a->lon, b->lat, b->lon);
     if(dist < 15)
     {
-        return(1);
+        return(dist);
     }
     else
     {
@@ -39,6 +44,10 @@ int checkAdjacency(node *a, node*b)
 
 void insert(node **nodes, node *node)
 {
+    //Nodes are placed into an "array" of nodes. Each node contains a list of integers.
+    //These integers represent the position/index of every node.
+    //This is done so that a path can be followed by checking a node's "connected" field,
+    //Choosing an int from that list, and using it as an index for the "nodes" set.
     int i = 0;
     //Insert new node into node array.
     while(nodes[i] != NULL)
@@ -47,15 +56,29 @@ void insert(node **nodes, node *node)
         if(checkAdjacency(nodes[i], node))//if adjacent, add index to current node adjacencies.
         {
             while(node->connected[j] != NULL)
+            //Add the existing node's indice to the current node's "connected" list.
             {
                 ++j;
             }
+            //Make an int pointer, add it to the set of int pointers (connected).
             int *num = malloc(sizeof(int));
             *num = i;
             node->connected[j] = num;
+            
+            //Add the current node's indice to the existing node's connected list.
+            int k = 0;
+            while(nodes[i]->connected[k] != NULL)
+            {
+                ++k;
+            }
+            //Make an int pointer, add it to the set of int pointers (connected).
+            int *newNodeNum = malloc(sizeof(int));
+            *newNodeNum = j;
+            nodes[i]->connected[k] = newNodeNum;
         }
         ++i;
     }
+    //Place the new node at the end of the array of existing nodes.
     nodes[i] = node;
 }
 
