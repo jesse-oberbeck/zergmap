102a7342c17163f5227a720f84368d8ab0e7e5d1
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2017-02-20 14:39:44 -0500

    Killed off warning messages, and deleted a ton of unused code.

--- mapperB.c
+++ mapperB.c
@@ -198,7 +198,7 @@ void printem(node *cur)
         printf("\npacket #%d ID: %d\n", count, cur->ID);
         edge *adj = cur->connected;
         //puts("NullCheck");
-        printf("adj addr: %p\n", cur->connected);
+        printf("adj addr: %p\n", (void*)cur->connected);
         while(adj != NULL)
         {
             //puts("NotNull");
@@ -288,7 +288,7 @@ node *leastAdj(node *root)
         }
         root = root->next;
     }
-    printf("LEAST: %p\n", least);
+    printf("LEAST: %p\n", (void*)least);
     return(least);
 }
 
@@ -312,7 +312,7 @@ node * shortestRoute(node *root)
         ++index;
         edge = edge->next;
     }
-    printf("shortest route: %p weight: %d\n", lowNode, lowest);
+    printf("shortest route: %p weight: %d\n", (void*)lowNode, lowest);
     return(lowNode);
 }
 
@@ -410,197 +410,7 @@ node *findShortest(node *n, node *end)
     }
     return(path);
 }
-/*
-void startPaths(node* start, node *end)
-{
-    printf("END ID: %d\n", end->ID);
-    puts("startPaths");
-    node *path1 = NULL;
-    int dist1 = 999999;//Arbitrary high placeholder.
 
-    node *path2 = NULL;
-    int dist2 = 999999;
-
-    //Increment when a path reaches end.
-    int paths = 0;
-
-    node *collisionPoint = NULL;
-
-    edge *e = start->connected;
-
-//Find the two paths from start closest to the end point.
-    while(e != NULL)
-    {
-        int c = checkDistance(e->node, end);
-        if(c == 0)
-        {
-            puts("END NEXT TO START");
-            ++paths;
-        }
-        else if(c < dist1 && dist1 < 999999 && c < dist2)
-        {
-            dist2 = dist1;
-            path2 = path1;
-            dist1 = c;
-            path1 = e->node;
-            printf("PATH 1: %d DIST: %d &\n", path1->ID, dist1);
-            printf("PATH 2: %d DIST: %d\n", path2->ID, dist2);
-        }
-        else if(c < dist1)
-        {
-            dist1 = c;
-            path1 = e->node;
-            printf("PATH 1: %d DIST: %d\n", path1->ID, dist1);
-        }
-        else if(c < dist2)
-        {
-            dist2 = c;
-            path2 = e->node;
-            printf("PATH 2: %d DIST: %d\n", path2->ID, dist2);
-        }
-        e = e->next;
-    }
-    if(endProbe(path1, end))
-    {
-        puts("path1 next to end");
-        ++paths;
-    }
-    if(path2 != NULL && endProbe(path2, end))
-    {
-        puts("path1 next to end");
-        ++paths;
-    }
-    if(paths > 1)
-    {
-        printf("PATHS FOUND AT BEGINNING\n");
-        return;
-    }
-//If a second path was not found, exit.
-    if(path2 == NULL)
-    {
-        puts("!!!!!!!!!!!!!!!!!!!!Path 2 is null...");
-        return;
-    }
-    path1->visited = 1;
-    path2->visited = 1;
-
-
-
-//While collision has not occurred and both paths have not been found.
-    while(collisionPoint == NULL && paths < 2)
-    {
-        puts(" ");
-        //If path 1 is the end, or is next to the end, increment paths.
-        if(endProbe(path1, end) || path1->ID == end->ID)
-        {
-            ++paths;
-            puts("path1 found!");
-        }
-        //If path2 is the end, or is next to the end, increment paths.
-        if(endProbe(path2, end) || path2->ID == end->ID)
-        {
-            ++paths;
-            puts("path2 found!");
-        }
-        if(paths > 1)
-        {
-            puts("PATHING DONE");
-            return;
-        }
-
-
-//Check for common nodes each time they advance.
-        node *common = commonCheck(path1, path2);
-        if(common != NULL)
-        {printf("p1: %d p2: %d common: %d\n", path1->ID, path2->ID, common->ID);}
-
-//Choose next for path1
-        edge *edge1 = path1->connected;
-        path1->visited = 1;
-        node *path1check = findShortest(path1, end);//Closest non visited node.
-        node *alternate1 = NULL;
-//Finds an alternate path. May not be closest to the end, but isn't shared.
-        while(edge1 != NULL)
-        {
-            //Find a node that is not a common node, and not the shortest node.
-            if(edge1->node != common && edge1->node != path1check && edge1->node->visited == 0)
-            {
-                alternate1 = edge1->node;
-            }
-            edge1 = edge1->next;
-        }
-        //If the route closest to end is not shared, take it.
-        if(path1check != common && path1check != NULL && path1check->visited == 0)
-        {
-            puts("first option");
-            path1 = path1check;
-        }
-        //If an alternate was found, 
-        else if(alternate1 != NULL)
-        {
-            puts("alternate");
-            path1 = alternate1;
-        }
-        else if(common != NULL && common->visited == 0)
-        {
-            puts("common");
-            path1 = common;
-        }
-        else
-        {
-            collisionPoint = path1;
-            printf("COLLISION: %d\n", collisionPoint->ID);
-        }
-        path1->visited = 1;
-        printf("path1: %d\n", path1->ID);
-
-
-//Choose next for path2
-        edge *edge2 = path2->connected;
-        path2->visited = 1;
-        node *path2check = findShortest(path2, end);//Closest non visited node.
-        node *alternate2 = NULL;
-        while(edge2 != NULL)
-        {
-            if(edge2->node != common && edge2->node != path1check)
-            {
-                alternate2 = edge2->node;
-            }
-            edge2 = edge2->next;
-        }
-        //If the route closest to end is not shared, take it.
-        if(path2check != common && path2check != NULL)
-        {
-            path2 = path2check;
-        }
-        else if(alternate2 != NULL)
-        {
-            path2 = alternate1;
-        }
-        else if(common != NULL && common->visited == 0)
-        {
-            path2 = common;
-        }
-        else
-        {
-            collisionPoint = path2;
-            printf("COLLISION: %d\n", collisionPoint->ID);
-        }
-        path2->visited = 1;
-        printf("path2: %d\n", path2->ID);
-    }
-    if(paths > 1)
-    {
-        puts("HAS REDUNDANT PATHS");
-        return;
-    }
-    else
-    {
-        puts("Not enough paths...");
-        return;
-    }
-}
-*/
 void
 destroy(
     node * root)
@@ -677,7 +487,7 @@ void startPaths(node* start, node *end)
     int dist1 = 999999;//Arbitrary high placeholder.
 
     node *path2 = NULL;
-    int dist2 = 999999;
+    //int dist2 = 999999;
 
     //Increment when a path reaches end.
     int paths = 0;
@@ -694,7 +504,7 @@ void startPaths(node* start, node *end)
         }
         else if(c < dist1)
         {
-            dist2 = dist1;
+            //dist2 = dist1;
             path2 = path1;
             dist1 = c;
             path1 = e->node;
